5cb0e09adf21fbda02c855f3970f65b9bd1c33e7
Javier Peralta <enarmonias@gmail.com>
2024-12-20 13:08:43 +0100

    Theme improved

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -71,14 +71,6 @@ const siteConfig: SiteConfig = {
         {
             text: 'Contact',
             href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
     ],
     socialLinks: [
@@ -96,8 +88,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: '',
+        text: '¡Hola! Me llamo Javi. Llevo unos cuantos años trabajando como desarrollador.',
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
